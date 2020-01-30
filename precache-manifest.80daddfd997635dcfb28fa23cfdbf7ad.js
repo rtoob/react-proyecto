4f171c5ef8c404f538b6247587723c2513f6a925
Your Name <rafaeltub@gmail.com>
2020-01-30 05:38:19 -0300

    Updates

---
+++ precache-manifest.80daddfd997635dcfb28fa23cfdbf7ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2156ca9509d1d192cf451f2f03a85f91",
+    "url": "/rtoob/react-proyecto/index.html"
+  },
+  {
+    "revision": "95109934ad10f502e551",
+    "url": "/rtoob/react-proyecto/static/css/main.4269ee80.chunk.css"
+  },
+  {
+    "revision": "e8658130c51183d8ec46",
+    "url": "/rtoob/react-proyecto/static/js/2.ff4a48c8.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/rtoob/react-proyecto/static/js/2.ff4a48c8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95109934ad10f502e551",
+    "url": "/rtoob/react-proyecto/static/js/main.a619dd45.chunk.js"
+  },
+  {
+    "revision": "a8d136cdbe6c6ed83a8d",
+    "url": "/rtoob/react-proyecto/static/js/runtime-main.b474e21a.js"
+  }
+]);
